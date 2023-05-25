498b993a5b6a417aef295141d0000593c2b5df29
this-is-sreehari <sreeharipathil@gmail.com>
2023-05-25 10:36:44 +0530

    added origin

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React from "react";
+import axios from "axios";
+import baseUrl from "./config";
 
 const App = () => {
+
+
+    axios(`${baseUrl}/api/v1/books`).then(books => {
+        console.log(books);
+
+    })  
+
+
+
     return <div>My first component</div>
 }
 
